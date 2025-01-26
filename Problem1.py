916fdc4328647dabb3b8835babade4d55c8f1116
hemanth-ks97 <hemanth.ks97@gmail.com>
2025-01-26 14:29:58 -0600

    Added python solution

---
+++ Problem1.py
@@ -0,0 +1,24 @@
+# Time : O(logN)
+# Space : O(1)
+
+def find_missing_number(nums):
+    l,r = 0, len(nums) - 1
+ 
+    expected_difference = nums[0]
+
+    while l <= r:
+        mid = l + (r-l)//2 
+
+        difference = abs(mid - nums[mid])
+
+        if difference > expected_difference:
+            # The missing number lies to the left of mid
+            r = mid - 1
+        else:
+            # The missing number lies to the right of mid
+            l = mid + 1
+    
+    if r == 0:
+        return nums[r] - 1
+    else:
+        return nums[r] + 1
